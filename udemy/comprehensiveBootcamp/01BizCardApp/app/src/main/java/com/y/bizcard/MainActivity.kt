bb902eb140b88a42be88efaec26295b6f3b38b1f
Y <hwiyul_g@daum.net>
2023-08-22 23:34:09 +0900

    Add : Profile Image 부분과 Divider로 아래 내용 부분 구분까지. Surface에 대한 학습 필요.

--- udemy/comprehensiveBootcamp/01BizCardApp/app/src/main/java/com/y/bizcard/MainActivity.kt
+++ udemy/comprehensiveBootcamp/01BizCardApp/app/src/main/java/com/y/bizcard/MainActivity.kt
@@ -3,13 +3,34 @@ package com.y.bizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults.cardColors
+import androidx.compose.material3.CardDefaults.cardElevation
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.y.bizcard.ui.theme.BizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,12 +50,69 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun CreateBizCard() {
+    // Surfcae is canvas
+    // screen에 보여지고 싶은 무언가를 create하고 싶을 때,
+    // surface를 사용함.
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            colors = cardColors(
+                containerColor = Color.White,
+            ),
+            elevation = cardElevation(
+                defaultElevation = 4.dp,
+            ),
+        ) {
+            CreateImageProfile()
+            Divider()
+
+        }
+
+    }
+}
+
+@Composable
+private fun CreateImageProfile(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(width = 0.5.dp, color = Color.LightGray),
+        tonalElevation = 4.dp,
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+    ) {
+        Column(
+            modifier = Modifier.height(300.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.profile_img),
+                contentDescription = "profile image",
+                modifier = Modifier.size(135.dp),
+                contentScale = ContentScale.Crop,
+            )
+
+        }
+    }
+}
 
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BizCardTheme {
-        Greeting("Android")
+        CreateBizCard()
     }
 }
