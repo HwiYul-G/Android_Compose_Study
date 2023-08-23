6fef58ca60982d95a700843ffd80cb4302929c73
Y <hwiyul_g@daum.net>
2023-08-23 18:31:00 +0900

    Add : Complete the BizCard App. - buttonClickeEvent를 위해 변경가능한 state를 remember해둠 - Card라는 것에서 modifier, shape, elevation 등의 기능을 사용해봄 - Row, Column, Box, Divider 등 기본 배치 사용 (할 것이 없을 때는 Empty한 Box를 배치하기도 함) - Content를 함수로 쪼개서 사용함 - LazyColumn으로 기존의 Recyclerview를 replace 해서 사용함을 배움 - CreateImaegProfile을 통해 같은 것은 함수로 끊어서 사용

--- udemy/comprehensiveBootcamp/01BizCardApp/app/src/main/java/com/y/bizcard/MainActivity.kt
+++ udemy/comprehensiveBootcamp/01BizCardApp/app/src/main/java/com/y/bizcard/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,21 +17,29 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults.cardColors
 import androidx.compose.material3.CardDefaults.cardElevation
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.y.bizcard.ui.theme.BizCardTheme
@@ -43,7 +54,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    CreateBizCard()
                 }
             }
         }
@@ -52,6 +63,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
+    val buttonClickedState = remember {
+        mutableStateOf(false)
+    }
     // Surfcae is canvas
     // screen에 보여지고 싶은 무언가를 create하고 싶을 때,
     // surface를 사용함.
@@ -73,11 +87,112 @@ fun CreateBizCard() {
                 defaultElevation = 4.dp,
             ),
         ) {
-            CreateImageProfile()
-            Divider()
+            Column(
+                modifier = Modifier.height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CreateImageProfile()
+                Divider()
+                CreateInfo()
+                Button(
+                    onClick = {
+                        buttonClickedState.value = !buttonClickedState.value
+                    }
+                ) {
+                    Text(text = "Portfolio", style = MaterialTheme.typography.bodySmall)
+                }
+            }
+
+            if (buttonClickedState.value) {
+                Content()
+            } else {
+                Box() {
+
+                }
+            }
 
         }
 
+    }
+}
+
+// @Preview
+@Composable
+fun Content() {
+    Box(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Surface(
+            modifier = Modifier
+                .padding(3.dp)
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2.dp, color = Color.LightGray),
+        ) {
+            Portfolio(data = listOf("Project1", "Project2", "Project3"))
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    // LazyColumn은 scrollable 하고, recyclable view의 장점을 가짐
+    // 보여지는 만큼 불러옴
+    LazyColumn {
+        items(data) { item ->
+            Card(
+                modifier = Modifier
+                    .padding(13.dp)
+                    .fillMaxWidth(),
+                shape = RectangleShape,
+                elevation = cardElevation(4.dp),
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .background(Color.White)
+                        .padding(7.dp)
+                ) {
+                    CreateImageProfile(modifier = Modifier.size(90.dp))
+                    Column(
+                        modifier = Modifier
+                            .padding(7.dp)
+                            .align(alignment = Alignment.CenterVertically)
+                    ) {
+                        Text(text = item, fontWeight = FontWeight.Bold)
+                        Text(text = "A great Project", style = MaterialTheme.typography.bodySmall)
+                    }
+                }
+
+            }
+        }
+
+    }
+}
+
+
+@Composable
+private fun CreateInfo() {
+    Column(modifier = Modifier.padding(5.dp)) {
+        Text(
+            text = "Miles P.",
+            style = MaterialTheme.typography.headlineLarge,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = "Android Compose Programmer",
+            modifier = Modifier.padding(3.dp),
+        )
+        Text(
+            text = "@themilesCompose",
+            modifier = Modifier.padding(3.dp),
+            style = MaterialTheme.typography.bodyMedium,
+        )
     }
 }
 
@@ -92,19 +207,13 @@ private fun CreateImageProfile(modifier: Modifier = Modifier) {
         tonalElevation = 4.dp,
         color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
     ) {
-        Column(
-            modifier = Modifier.height(300.dp),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.profile_img),
-                contentDescription = "profile image",
-                modifier = Modifier.size(135.dp),
-                contentScale = ContentScale.Crop,
-            )
+        Image(
+            painter = painterResource(id = R.drawable.profile_img),
+            contentDescription = "profile image",
+            modifier = modifier.size(135.dp),
+            contentScale = ContentScale.Crop,
+        )
 
-        }
     }
 }
 
